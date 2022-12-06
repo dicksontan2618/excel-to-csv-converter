685a420b294013f28fad3524d44c029257d1c90e
dicksontan <dicksontan2618@gmail.com>
2022-12-06 23:18:14 +0800

    add error page

---
+++ error.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <!-- <script src="https://code.jquery.com/jquery-3.6.1.slim.min.js" integrity="sha256-w8CvhFs7iHNVUtnSP0YKEg00p9Ih13rlL9zGqvLdePA=" crossorigin="anonymous"></script> -->
+    <link href="https://www.dafontfree.net/embed/Z291ZHktaGFuZHRvb2xlZC1yZWd1bGFyJmRhdGEvMjUvZy8xMjc2MjYvR291ZHlIYW5kdG9vbGVkLnR0Zg" rel="stylesheet" type="text/css" />
+    <title>CAT201 Assignment</title>
+
+</head>
+
+<body>
+    <section class="sec" id="sec-3">
+        <div class="wrapper" id="wrapper-2">
+            <div class="error">
+                <div class="invalid-page">
+                    <div class="invalid-picture">
+                        <img src="images/file.png" height="400px">
+                    </div>
+                    <div class="invalid">
+                        <p>Invalid<br>
+                        <p> File!</p>
+                    </div>
+                </div>
+                <div class="valid">
+                    <p>Please use a valid Excel (.xsl/.xslx) file!</p>
+                </div>
+                <div class="ornament" id="ornament-1">
+                    <img class="ornament-obj" src="images/ornament-1.png" height="200px">
+                    <div class="button" id="button-words-1">
+                        <a href="index.html" class="return" style="text-decoration:none;color: transparent;">
+                            Home
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+    <script src="script.js"></script>
+</body>
+
+</html>
